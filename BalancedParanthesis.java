52fcf892c1151ce86e4f66acb40971be3f0b27a0
jaidevxr <jaiy9956@gmail.com>
2025-10-06 12:21:39 +0530

    adding file

---
+++ BalancedParanthesis.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class BalancedParanthesis {
+    static boolean isBalanced(String str)
+    {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<str.length();i++)
+        {
+            char ch = str.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
+                stack.push(ch);
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                char top = stack.pop();
+                if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='['))
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
